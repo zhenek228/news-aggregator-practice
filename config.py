6682bfa43550b0bab804b2550865712ea447d997
Yevhenii <zhenyabs82@gmail.com>
2025-05-26 17:32:00 +0300

    Update config.py with new settings

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 STUDENT_ID = "Lavrinenko_c3477004"
 SOURCES = [
     "https://feeds.bbci.co.uk/news/rss.xml",
-    "https://edition.cnn.com/",
-    "https://www.reuters.com/",
-    "https://www.nytimes.com/",
-    "https://www.theguardian.com/",
+    "https://rss.cnn.com/rss/cnn_topstories.rss",
+    "https://feeds.reuters.com/reuters/topNews",
+    "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+    "https://www.theguardian.com/world/rss",
    
 ]
 
